fda64f4828028d6c2d3a3728caee4155c78216c6
jmchza <manuel.chouza@storyxag.com>
2018-06-18 09:50:58 +0200

    updates

--- db/clients.js
+++ db/clients.js
@@ -3,18 +3,19 @@
 const clients = [
   { id: '1', name: 'Samplr', clientId: 'abc123', clientSecret: 'ssh-secret', isTrusted: false },
   { id: '2', name: 'Samplr2', clientId: 'xyz123', clientSecret: 'ssh-password', isTrusted: true },
+  { id: '3', name: 'Samplr3', clientId: 'admin', clientSecret: 'faktoora2017', isTrusted: true },
 ];
 
 module.exports.findById = (id, done) => {
   for (let i = 0, len = clients.length; i < len; i++) {
     if (clients[i].id === id) return done(null, clients[i]);
   }
-  return done(new Error('Client Not Found'));
+  return done(new Error('findById::Client Not Found'));
 };
 
 module.exports.findByClientId = (clientId, done) => {
   for (let i = 0, len = clients.length; i < len; i++) {
     if (clients[i].clientId === clientId) return done(null, clients[i]);
   }
-  return done(new Error('Client Not Found'));
+  return done(new Error('findByClientId::Client Not Found'));
 };
